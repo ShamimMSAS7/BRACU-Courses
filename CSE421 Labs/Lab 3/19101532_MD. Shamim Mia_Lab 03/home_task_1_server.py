433c3ecd21dcc90d85585b8311dbad4bbec749e8
Shamim Saif <shamimmsas7@gmail.com>
2022-10-18 11:48:53 +0600

    Adding the contents of CSE421 Labs

---
+++ CSE421 Labs/Lab 3/19101532_MD. Shamim Mia_Lab 03/home_task_1_server.py
@@ -0,0 +1,39 @@
+#ID: 19101532
+# Name: MD. Shamim Mia
+
+import socket
+
+HEADER = 16
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "End"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+server.listen()
+print("Server is listening")
+conn, addr = server.accept()
+connected = True
+
+while connected:
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+            conn.send("Goodbye".encode(FORMAT))
+        else:
+            hours = int(msg)
+            salary = 0
+            if hours <= 40:
+                salary = hours*200
+            else:
+                salary = 8000 + (hours-40)*300
+            conn.send(str(salary).encode(FORMAT))
+
+
+conn.close()
